4c44fc89c8fdded0f250e10730ff8ba52f95d934
hiru daloi <daloihiru8@gmail.com>
2025-08-14 10:42:21 +0000

    update engine oil price per liter

--- src/lib/parts-data.ts
+++ src/lib/parts-data.ts
@@ -18,10 +18,10 @@ export const allParts: Part[] = [
   { name: "Transfer Case Oil", price: 1200 },
   { name: "Differential Oil", price: 1000 },
   { name: "Diesel Filter", price: 1200 },
-  { name: "ECSTAR PREMIUM 0W20-SHELL", price: 400 }, // Assuming per liter, adjust if needed
-  { name: "MGGO(75W90)-SHELL", price: 450 }, // Assuming per liter, adjust if needed
-  { name: "SYNTHETIC OIL (0W40)-IOCL", price: 900 }, // Assuming per liter, adjust if needed
+  { name: "ECSTAR PREMIUM 0W20-SHELL", price: 1120.33 },
+  { name: "MGGO(75W90)-SHELL", price: 386.44 },
+  { name: "SYNTHETIC OIL (0W40)-IOCL", price: 987.28 },
   { name: "ECSTAR PETROL 0W16 - SHELL", price: 406.77 },
   { name: "ECSTAR PETROL 0W20 - IOCL", price: 390.67 },
-  { name: "ECSTAR DIESEL 5W30-IOCL", price: 500 }, // Assuming per liter, adjust if needed
+  { name: "ECSTAR DIESEL 5W30-IOCL", price: 524.57 },
 ];
