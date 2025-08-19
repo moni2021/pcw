8e99820093c9920dd3a4131631dace125326c2e9
hiru daloi <daloihiru8@gmail.com>
2025-08-19 04:33:35 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    esmExternals: 'loose', 
-    serverActions: true,
+    serverActions: {},
   },
   typescript: {
     ignoreBuildErrors: true,
