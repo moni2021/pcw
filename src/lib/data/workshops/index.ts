b52379567399f9f77e8611a57f8eda57d6e655c8
hiru daloi <daloihiru8@gmail.com>
2025-08-23 06:18:12 +0000

    Any other workshop which were removed

--- src/lib/data/workshops/index.ts
+++ src/lib/data/workshops/index.ts
@@ -3,4 +3,5 @@ import { Workshop } from '../../types';
 export const workshops: Workshop[] = [
     { id: 'arena-bijoynagar', name: 'ARENA - BIJOYNAGAR (KAMRUP)', city: 'BIJOYNAGAR' },
     // { id: 'sow-azara', name: 'SOW - AZARA OPP.SABHAGHAR-SRV', city: 'AZARA' },
+    // { id: 'nexa-bijoynagar', name: 'NEXA - BIJOYNAGAR', city: 'BIJOYNAGAR' },
 ];
