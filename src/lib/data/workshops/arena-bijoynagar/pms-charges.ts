05f38f91e91ac957346733ccb5174960f5cd0ad5
hiru daloi <daloihiru8@gmail.com>
2025-08-23 05:17:13 +0000

    add this pms price data to bijoynagar as per model vise and service type

--- src/lib/data/workshops/arena-bijoynagar/pms-charges.ts
+++ src/lib/data/workshops/arena-bijoynagar/pms-charges.ts
@@ -15,14 +15,33 @@ const pmsChargesRaw: Omit<PmsCharge, 'workshopId' | 'id'>[] = [
         { model: "Alto 800", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1480 },
         { model: "Ertiga", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1720 },
         { model: "Brezza", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1675 },
-        { model: "Eeco", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1340 }, // Mapping OMNI and MARUTI EECO PETROL
+        { model: "Eeco", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1340 },
         { model: "Baleno", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1520 },
         { model: "Celerio", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1480 },
         { model: "Ignis", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1535 },
         { model: "XL6", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1720 },
         { model: "Super Carry", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 600 },
-        { model: "S-Presso", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1480 }, // Assuming S-Presso is similar to Alto
-        { model: "Ritz", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1535 }, // Assuming Ritz is similar to Swift
+        { model: "S-Presso", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1480 },
+        { model: "Ritz", labourCode: "PMS-GROUP-1", labourDesc: service, basicAmt: 1535 },
+    ]),
+    // --- Paid Service Group (20k, 30k, 40k, 60k, 80k, 90k) ---
+    ...["Paid Service (20,000 km)", "Paid Service (30,000 km)", "Paid Service (40,000 km)", "Paid Service (60,000 km)", "Paid Service (80,000 km)", "Paid Service (90,000 km)"].flatMap(service => [
+        { model: "Dzire", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1515 },
+        { model: "Swift", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1515 },
+        { model: "Eeco", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1320 },
+        { model: "Alto K10", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1460 },
+        { model: "Alto 800", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1460 },
+        { model: "Wagon R", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1460 },
+        { model: "Baleno", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1500 },
+        { model: "Celerio", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1460 },
+        { model: "S-Presso", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1460 },
+        { model: "Brezza", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1655 },
+        { model: "Ignis", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1515 },
+        { model: "Ertiga", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1700 },
+        { model: "Fronx", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1500 },
+        { model: "Ciaz", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1615 },
+        { model: "XL6", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 1700 },
+        { model: "Super Carry", labourCode: "PMS-GROUP-2", labourDesc: service, basicAmt: 600 },
     ]),
 ];
 
@@ -31,4 +50,7 @@ const pmsCharges: Omit<PmsCharge, 'workshopId'>[] = pmsChargesRaw.map(charge => 
     id: `${workshopId}-${charge.model}-${charge.labourDesc}`.toLowerCase().replace(/[^a-z0-9-]/g, '')
 }));
 
-export default pmsCharges;
+// Remove duplicates, giving preference to the last entry for a given id
+const uniquePmsCharges = Array.from(new Map(pmsCharges.map(item => [item.id, item])).values());
+
+export default uniquePmsCharges;
